3814d67ca77acd4f3ba89748675b5bf2fedff601
Tatu Peltola <tatu.peltola@gmail.com>
2016-12-10 21:11:24 +0200

    Change to left aligned mode

--- digimoppe/digimoppe.ino
+++ digimoppe/digimoppe.ino
@@ -2,10 +2,10 @@ void setup() {
   DDRB |= (1<<4) | (1<<3); // PB4 = digital pin 12, PB3 = digital pin 11
   PORTB = 0;
   Serial.begin(1000000);
-  ADMUX = (3<<REFS0) | (0<<MUX0);
+  ADMUX = (3<<REFS0) | (1<<ADLAR) | (0<<MUX0); // left aligned: 8-bit result on ADCH
   ADCSRB = (0<<ADTS0); // free running mode
   DIDR0 = (1<<ADC0D);
-  ADCSRA = (1<<ADIE) | (1<<ADATE) | (4<<ADPS0); // interrupt, auto trigger, prescaler 16
+  ADCSRA = (1<<ADIE) | (1<<ADATE) | (5<<ADPS0); // interrupt, auto trigger, prescaler 32
 
   sei();
   // enable ADC and start conversion last
@@ -19,7 +19,16 @@ void loop() {
 
 ISR(ADC_vect) {
   PORTB ^= 1<<4; // PB4 = digital pin 12
-  UDR0 = ADCL;
-  ADCH; // ADCH must be read after ADCL
+#if 0
+  uint8_t adl, adh;
+  adl = ADCL;
+  adh = ADCH; // ADCH must be read after ADCL
+  UDR0 = adl;
+  while(! (UCSR0A & UDRE0) ); // wait for empty USART data register
+  UDR0 = adh;
+#else
+  ADCL;
+  UDR0 = ADCH;
+#endif
 }
 
